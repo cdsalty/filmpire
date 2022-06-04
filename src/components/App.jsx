0dd370107cc3800604294366fd8a2e4729bea9f5
cdsalty <soltiscd@gmail.com>
2022-06-04 13:58:33 -0400

    added dependencies needed

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { CssBaseline } from "@mui/material"; // Kickstart an elegant, consistent, and simple baseline to build upon.
+// import { Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
